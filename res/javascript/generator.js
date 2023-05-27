6d416f1c20f8b6db19a9d2b8a3d867de9ffcaac1
Adriel007 <adrielsouzaandrade8@gmail.com>
2023-05-27 08:53:49 -0300

    commit

--- res/javascript/generator.js
+++ res/javascript/generator.js
@@ -1,26 +1,40 @@
 function send() {
-    const texts = document.querySelectorAll("div textarea");
+    const groups = [];
+
+    for (const value of document.querySelectorAll(".div-textareas fieldset")) {
+        const json = {
+            title: value.children[0].textContent,
+            array: []
+        };
+
+        for (let c = 0; c < value.children[1].children.length; c++)
+            json.array.push(value.children[1].children[c].value);
+
+        groups.push(json);
+    }
+
     const result = document.querySelector("textarea[readonly]");
     const string = document.getElementById("string").value;
-    let str = "";
 
-    texts.forEach(text => str += text.value + "@separatorphp@");
-
-    const textEncoder = new TextEncoder();
-    const utf8Bytes = textEncoder.encode(str);
-    const file = new Blob([utf8Bytes], { type: 'text/plain;charset=utf-8' });
+    const file = new Blob([JSON.stringify(groups)], { type: 'application/json;charset=utf-8' });
     const formData = new FormData();
     formData.append('file', file);
-
     formData.append('string', string);
     fetch('../php/scripts.php', {
         method: 'POST',
         body: formData
     })
         .then(response => response.json())
-        .then(data =>
-            result.value = data.result
-        )
+        .then(data => {
+            data = data.result;
+            console.log(data)
+            let str = "Seu texto foi classificado na seguinte ordem:\n";
+            for (const key in data) {
+                console.log(key)
+                str += `${key}: ${(data[key] * 100).toFixed(3)}%\n`;
+            }
+            result.value = str;
+        })
         .catch(error => {
             console.error('Erro:', error);
         });
@@ -41,9 +55,13 @@ function newGroup() {
 
     group.legend.textContent = "Grupo " + counter;
     group.legend.contentEditable = "true";
-    group.textarea.placeholder = "Insira seu texto que representa um grupo aqui...";
+    group.textarea.placeholder = "Insira um texto para o treinamento...";
     group.less.textContent = "-";
+    group.less.onclick = removeTextArea;
+    group.less.classList.add("gp-" + counter);
     group.more.textContent = "+";
+    group.more.onclick = newTextArea;
+    group.more.classList.add("gp-" + counter);
 
     container.appendChild(group.fieldset);
 
@@ -64,32 +82,24 @@ function removeGroup() {
         container.children[container.children.length - 1].remove();
 }
 
-function newTextArea() {
-    const textarea = document.createElement("textarea");
-    const container = document.querySelector("div");
+function newTextArea(element) {
+    if (element?.target)
+        element = element.target;
 
-    textarea.classList.add("optional");
-    textarea.placeholder = "Insira seu texto aqui...";
+    const counter = element.classList[0].split("-")[1];
+    const container = document.querySelectorAll(".div-textareas fieldset")[counter - 1].children[1];
+    const textarea = document.createElement("textarea");
+    textarea.placeholder = "Insira um texto para o treinamento...";
     container.appendChild(textarea);
 }
 
-function removeTextArea() {
-    const textarea = document.getElementsByClassName("optional");
+function removeTextArea(element) {
+    if (element?.target)
+        element = element.target;
 
-    if (textarea.length > 0)
-        textarea[textarea.length - 1].remove();
-}
+    const counter = element.classList[0].split("-")[1];
+    const container = document.querySelectorAll(".div-textareas fieldset")[counter - 1].children[1];
 
-function range(id) {
-    const coherence = document.getElementById("coherence");
-    const creative = document.getElementById("creative");
-
-    switch (id) {
-        case "coherence":
-            creative.value = 10 - coherence.value;
-            break;
-        case "creative":
-            coherence.value = 10 - creative.value;
-            break;
-    }
+    if (container.children.length >= 2)
+        container.children[container.children.length - 1].remove();
 }
--- res/php/scripts.php
+++ res/php/scripts.php
@@ -5,13 +5,10 @@ include(dirname(__DIR__) . '/machine-learning/naive-bayes.php');
 error_reporting(0);
 
 if (isset($_FILES['file'])) {
-    $string = $_POST['string'];
     $tmp = $_FILES['file']['tmp_name'];
-    $content = file_get_contents($tmp);
-    $array = explode('@separatorphp@', $content);
-    array_pop($array);
-
-    $jsonResult = json_encode(['result' => naive_bayes($array, $string)['type']], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+    $content = json_decode(file_get_contents($tmp), true);
+    $string = $_POST['string'];
+    $jsonResult = json_encode(['result' => naive_bayes($content, $string)], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
 
     if ($jsonResult === false)
         echo json_encode(['error' => json_last_error_msg()], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
@@ -22,27 +19,12 @@ if (isset($_FILES['file'])) {
 
 function naive_bayes($dataset, $phrase)
 {
-    $data = [
-        'all' => [],
-        'type' => null
-    ];
+    $data = null;
     $classifier = new Naive_Bayes();
     if ($phrase !== false) {
-        $keys = array_keys($dataset);
-
-        $len = count($keys);
-        for ($c = 0; $c < $len; $c++) {
-            foreach ($dataset[$keys[$c]] as $value)
-                $classifier->train($keys[$c], $value);
-
-            $groups = $classifier->classify($phrase);
-            if ($groups[$keys[$c]] * 100 >= 1)
-                array_push($data['all'], $phrase . ' -> ' . $groups[$keys[$c]] . ' -> ' . $keys[$c]);
-        }
-
-        $type = array_search(max($groups), $groups);
-        $data['type'] = $type;
-        return $data;
+        foreach ($dataset as $value)
+            $classifier->train($value['title'], $value['array']);
+        $data = $classifier->classify($phrase);
     }
 
     return $data;
